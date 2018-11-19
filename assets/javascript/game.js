79d373f0f099d0b6d6d1c4f2d8bfb238eab8661e
jonDatz <notjondatz@gmail.com>
2018-11-19 00:05:40 -0500

    Added non-repetitive wrong guesses, properly counts down remaining guesses, pushes array to DOM, moving to v0.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,79 @@
 
 var hmWord = ['squirtle', 'charmander', 'bulbasaur'];
 
+// For repeat letters
+
+var doubleWord = ['a', 'b', 'c',
+    'd', 'e', 'f',
+    'g', 'h', 'i',
+    'j', 'k', 'l',
+    'm', 'n', 'o',
+    'p', 'q', 'r',
+    's', 't', 'u',
+    'v', 'w', 'x',
+    'y', 'z'
+];
+
 // choose a word randomly
 var hmNumber = Math.floor(Math.random() * hmWord.length);
 var pickWord = hmWord[hmNumber];
+var lettersInWord = [];
 var underScore = [];
+var rightWord = [];
+var wrongWord = [];
+
+
+var guessesLeft = 10;
+var wins = 0;
+var losses = 0;
+
+
+// RESET
+
+function reset() {
+    var hmNumber = Math.floor(Math.random() * hmWord.length);
+    var pickWord = hmWord[hmNumber];
+    var lettersInWord = [];
+    var underScore = [];
+    var rightWord = [];
+    var wrongWord = [];
+    var doubleWord = ['a', 'b', 'c',
+    'd', 'e', 'f',
+    'g', 'h', 'i',
+    'j', 'k', 'l',
+    'm', 'n', 'o',
+    'p', 'q', 'r',
+    's', 't', 'u',
+    'v', 'w', 'x',
+    'y', 'z'
+    ];
+
+}
+
+
+
+
+
+
+// Working on the DOM
+
+
+// var domUnderscores = document.getElementById('underscores');
 
 console.log(pickWord);
 // Create underscores based on word length
 
 function makeUnderscores() {
+
+    lettersInWord = pickWord.split('');
+    console.log(lettersInWord);
+
     for (var i = 0; i < pickWord.length; i++) {
+
         underScore.push('_');
+
+        document.getElementById('underscores').innerHTML = underScore.join(' ');
+
     }
     return underScore;
 }
@@ -27,20 +89,52 @@ document.onkeyup = function (event) {
     console.log(userGuess);
 
     // check if keypress matches the selected word
-    if(pickWord.indexOf(userGuess) > -1){
+    if (pickWord.indexOf(userGuess) > -1) {
         console.log(true);
+
+        // add to right words array
+        rightWord.push(userGuess);
+
+        underScore[pickWord.indexOf(userGuess)] = userGuess;
+        console.log(underScore);
+
+        //Writes initial underscores to screen, removes commas with .join(' ')
+        document.getElementById('underscores').innerHTML = underScore.join(' ');
+
+    } else {
+
+        // if the incorrect letter is already guessed
+        if (wrongWord.includes(userGuess)) {
+
+            // feedback to check success
+            console.log('Wrong guess already answered');
+
+        } else if (guessesLeft > 0) {
+
+
+
+            // If incorrect letter given, push to wrongWord array
+            wrongWord.push(userGuess);
+
+            //subtract from guesses left before losing
+            guessesLeft--;
+            console.log(guessesLeft);
+
+            // Write to html the incorrect guesses & how many guesses are 
+            document.getElementById('guessesMade').innerHTML = wrongWord.join(', ');
+            document.getElementById('remainingGuesses').innerHTML = guessesLeft;
+
+        } else {
+
+            document.getElementById('remainingGuesses').innerHTML = "You lose!";
+            losses++;
+            document.getElementById('losses').innerHTML = losses;
+            reset();
+        }
 
     }
 
-    // Tut Vid: ttps://www.youtube.com/watch?v=f5BbzXgvi1o
-    // Timestamp: 56:26
-    // Check if user has typed key in already
-    // If so, do nothing, dont take from guesses left
 
-    // else new guess
-
-
-    // or wrong
 }
 
 
